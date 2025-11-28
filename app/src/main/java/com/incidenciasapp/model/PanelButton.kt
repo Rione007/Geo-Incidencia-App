5a088928dc2b1dbda550e97cf8eba54ea2378798
Rione <anderson020398.007@gmail.com>
2025-11-28 00:30:33 -0500

    agregado los fragments

---
+++ app/src/main/java/com/incidenciasapp/model/PanelButton.kt
@@ -0,0 +1,9 @@
+package com.incidenciasapp.model
+
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+
+data class PanelButton(
+    val container: LinearLayout,
+    val fragment: Fragment
+)
--- app/src/main/java/com/incidenciasapp/ui/PanelPrincipalActivity.kt
+++ app/src/main/java/com/incidenciasapp/ui/PanelPrincipalActivity.kt
@@ -1,23 +1,84 @@
 package com.incidenciasapp.ui
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.incidenciasapp.R
+import com.incidenciasapp.model.PanelButton
+import com.incidenciasapp.ui.fragments.ExplorarFragment
+import com.incidenciasapp.ui.fragments.IncidentesFragment
+import com.incidenciasapp.ui.fragments.MisReportesFragment
+import com.incidenciasapp.ui.fragments.PerfilFragment
 
 class PanelPrincipalActivity : AppCompatActivity() {
+
+    private lateinit var btnExplorar: LinearLayout
+    private lateinit var btnIncidentes: LinearLayout
+    private lateinit var btnMisReportes: LinearLayout
+    private lateinit var btnPerfil: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_panel_principal)
 
+        btnExplorar = findViewById(R.id.btnExplorar)
+        btnIncidentes = findViewById(R.id.btnIncidentes)
+        btnMisReportes = findViewById(R.id.btnMisReportes)
+        btnPerfil = findViewById(R.id.btnPerfil)
+
+        val buttons = listOf(
+            PanelButton(btnExplorar, ExplorarFragment.newInstance()),
+            PanelButton(btnIncidentes, IncidentesFragment.newInstance()),
+            PanelButton(btnMisReportes, MisReportesFragment.newInstance()),
+            PanelButton(btnPerfil, PerfilFragment.newInstance())
+        )
+
+        loadFragment(ExplorarFragment.newInstance())
+        setActiveButton(btnExplorar)
+
+        buttons.forEach { button ->
+            button.container.setOnClickListener {
+                val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+                if (currentFragment?.javaClass == button.fragment.javaClass) return@setOnClickListener
+                loadFragment(button.fragment)
+                setActiveButton(button.container)
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
+    private fun setActiveButton(activeButton: LinearLayout) {
+        val allButtons = listOf(btnExplorar, btnIncidentes, btnMisReportes, btnPerfil)
+        allButtons.forEach { button ->
+            val isActive = button == activeButton
+            button.isSelected = isActive
+            val iconView = button.getChildAt(0) as? ImageView
+            val textView = button.getChildAt(1) as? TextView
+            if (isActive) {
+                iconView?.setColorFilter(getColor(R.color.color_active_text))
+                textView?.setTextColor(getColor(R.color.color_active_text))
+            } else {
+                iconView?.setColorFilter(getColor(R.color.color_inactive_text))
+                textView?.setTextColor(getColor(R.color.color_inactive_text))
+            }
         }
     }
 }
